010a3f16b568b33d2d1faf2dd6f82cd44d8cbfb4
Teo Oliver <teo.liz.oli@gmail.com>
2020-09-12 19:29:19 +0100

    refactor User.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("hi there")
+import { User } from "./models/User";
+
+const user = new User({ id: 10 });
+
+user.events.on("change", () => {
+  console.log("change");
+});
+
+user.events.trigger("change");
