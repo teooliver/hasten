3f17e47c83b618cd6e4d4a2afb753d0bc78a5c10
Teo Oliver <teo.liz.oli@gmail.com>
2020-09-13 16:56:50 +0100

    finish User implementation

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { User } from "./models/User";
 
-const user = new User({ id: 10 });
+const user = new User({ id: 1, name: "newer name", age: 0 });
 
-user.events.on("change", () => {
-  console.log("change");
+user.on("save", () => {
+  console.log(user);
 });
 
-user.events.trigger("change");
+user.save();
